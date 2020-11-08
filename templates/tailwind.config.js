ef9de3c2cd7ee78a09ff470a5903171eae5ff629
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-11-08 11:17:24 -0500

    📄 docs+fix: comment to explain the modified purge extractor

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -2,9 +2,16 @@ const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
 
 module.exports = {
 	purge: {
-		content: ["./src/**/*.html", "./src/**/*.svelte"],
+		content: [
+			"./src/**/*.html",
+			"./src/**/*.svelte"
+		],
 		options: {
-			defaultExtractor: (content) => [...tailwindExtractor(content), ...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+			defaultExtractor: (content) => [
+				...tailwindExtractor(content),
+				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+			],
 			keyframes: true,
 		},
 	},
