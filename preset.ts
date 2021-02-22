75eeac36108a1ed7108f4ff194d5788e66c428cf
GrygrFlzr <grygrflzr@hotmail.com>
2021-02-22 07:17:44 +0700

    Revert "Use tailwind.config.cjs as config"
    This reverts commit 37089c9a347a289ce6bb78545fd9214907054ed2.

--- preset.ts
+++ preset.ts
@@ -7,7 +7,7 @@ const globalCSS = `@tailwind base;
 const addTailwind = (otherPlugins) => `plugins: [
 		// Some plugins, like postcss-nested, need to run before Tailwind
 		
-		tailwindcss("./tailwind.config.cjs"),
+		tailwindcss,
 		
 		// But others, like autoprefixer, need to run after
 
