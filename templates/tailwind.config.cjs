2ef532ba8554c2851f853c00c64f29dda4203b22
Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-03-17 15:24:16 -0400

    🎨 style+feat: clean up `tailwindExtractor` comments and support more purge paths (JS and TS)

--- templates/tailwind.config.cjs
+++ templates/tailwind.config.cjs
@@ -3,15 +3,11 @@ const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
 module.exports = {
 	purge: {
 		content: [
-			"./src/**/*.html",
-			"./src/**/*.svelte"
+			"./src/**/*.{html,js,svelte,ts}",
 		],
 		options: {
 			defaultExtractor: (content) => [
-				// This is an internal Tailwind function that we're not supposed to be allowed to use
-				// So if this stops working, please open an issue at
-				// https://github.com/svelte-add/tailwindcss/issues
-				// rather than bothering Tailwind Labs about it
+				// If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
 				...tailwindExtractor(content),
 				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
 				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
