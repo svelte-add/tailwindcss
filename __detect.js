7aee9afaae124b6579326d1944d50e2bc116d89b
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-09-18 17:23:12 -0400

    ⚡️ feat: detect tailwind directives in `src/app.postcss`

--- __detect.js
+++ __detect.js
@@ -1,3 +1,5 @@
+import { extension } from "../postcss/stuff.js";
+
 /** @type {import("../..").Heuristic[]} */
 export const heuristics = [
 	{
@@ -11,6 +13,17 @@ export const heuristics = [
 		async detector({ readFile }) {
 			const { text } = await readFile({ path: "/postcss.config.cjs" });
 			return text.includes("tailwindcss");
+		},
+	},
+	{
+		description: `\`@tailwind\` directives are used in \`src/app.${extension}\``,
+		async detector({ readFile }) {
+			const { text } = await readFile({ path: `/src/app.${extension}` });
+			if (!text.includes("@tailwind base")) return false;
+			if (!text.includes("@tailwind components")) return false;
+			if (!text.includes("@tailwind utilities")) return false;
+
+			return true;
 		},
 	},
 ];
