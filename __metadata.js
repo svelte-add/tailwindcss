edd1532eba5ff9c469a3f689ab773ac615105cc7
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-12-28 15:03:48 -0500

    📄 docs: language change when asking about plugins

--- __metadata.js
+++ __metadata.js
@@ -7,11 +7,11 @@ export const options = {
 	forms: {
 		context: "https://github.com/tailwindlabs/tailwindcss-forms",
 		default: false,
-		question: "Do you want to install the Tailwind CSS Forms plugin?",
+		question: "Do you want to use the Tailwind CSS Forms plugin?",
 	},
 	typography: {
 		context: "https://github.com/tailwindlabs/tailwindcss-typography",
 		default: false,
-		question: "Do you want to install the Tailwind CSS Typography plugin?",
+		question: "Do you want to use the Tailwind CSS Typography plugin?",
 	},
 };
